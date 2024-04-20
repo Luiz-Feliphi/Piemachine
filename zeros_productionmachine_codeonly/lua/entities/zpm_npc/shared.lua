7cb63e90f8ee81fcdf3ce340f997f25e333fd39a
ClemensProduction <clemensproduction@gmail.com>
2024-04-20 16:14:21 +0200

    Delete zeros_productionmachine_codeonly directory

--- zeros_productionmachine_codeonly/lua/entities/zpm_npc/shared.lua
+++
@@ -1,7 +0,0 @@
-ENT.Base 					= "base_ai"
-ENT.Type 					= "ai"
-ENT.PrintName 				= "Product Buyer"
-ENT.Category 				= "Zeros Productionmachine"
-ENT.Spawnable 				= true
-ENT.AdminSpawnable 			= true
-ENT.AutomaticFrameAdvance 	= true

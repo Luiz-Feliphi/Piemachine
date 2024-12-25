b2abc5ff193ef883139077fab5870f5e7d4a32cb
Luiz-Feliphi <108127347+Luiz-Feliphi@users.noreply.github.com>
2024-12-25 15:09:04 -0300

    Create sh_language_ptbr.lua
    PT-BR Translation by: POP PIC

---
+++ zeros_productionmachine_codeonly/lua/zpm/language/sh_language_ptbr.lua
@@ -0,0 +1,42 @@
+zpm = zpm or {}
+zpm.language = zpm.language or {}
+
+if zpm.config.SelectedLanguage ~= "ptbr" then return end
+
+zpm.language["BuildkitBuild_Info"] = "Por favor, limpe está área!"
+zpm.language["BuildkitBuild_Abort"] = "Algo esta no caminho!"
+zpm.language["ConstructionCompleted"] = "Construção concluída!"
+// zpm.language["CantInteract"] = "Você possui permissão para interagir com isso!"
+
+zpm.language["CantInteract_job"] = "Você não tem a job certa para usar esta máquina!"
+zpm.language["CantInteract_owner"] = "Você não possui esta máquina!"
+
+zpm.language["NoProductSelected"] = "Selecione um produto primeiro!"
+zpm.language["NoFuel"] = "A máquina possui combustível!"
+zpm.language["Start"] = "Iniciar"
+zpm.language["Stop"] = "Parar"
+zpm.language["Machine"] = "Máquina"
+zpm.language["Progress"] = "Progresso"
+zpm.language["Cycle"] = "Ciclo"
+zpm.language["Status"] = "Status"
+zpm.language["Inventory"] = "Inventário"
+zpm.language["Selected Product"] = "Produto selecionado"
+
+zpm.language["Ingredients"] = "Ingredientes"
+zpm.language["Recipies"] = "Receitas"
+zpm.language["Fuel"] = "Combustível"
+zpm.language["Refill"] = "Reabastecer"
+zpm.language["Refill Cost"] = "Custo para Reabastecer"
+
+zpm.language["FuseBox"] = "Caixa de fusíveis"
+zpm.language["Replace > E"] = "Repor > E"
+zpm.language["Replacing Fuse..."] = "Substituindo fusível..."
+
+zpm.language["FIXED"] = "CONSERTADO"
+zpm.language["CODE"] = "CÓDIGO"
+zpm.language["Waste"] = "Desperdiçar"
+zpm.language["Clear"] = "Limpar"
+
+zpm.language["Change Recipe"] = "Alterar Receita"
+zpm.language["Sell for"] = "Vender por"
+zpm.language["npc_sell_noproduct"] = "Volte quando tiver algo para mim ok."
